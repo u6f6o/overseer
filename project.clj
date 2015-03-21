a6a506c346a4bcca61e78796350c7f10689b4a4a
Ulf Gitschthaler <ulf@gitschthaler.de>
2015-03-21 17:54:59 +0100

    add leiningen to project

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/u6f6o/overseer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.csv "0.1.2"]]
   :main ^:skip-aot overseer.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
