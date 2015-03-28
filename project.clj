f6beebac4ce0d605720d36b2e6ad2d6b085266f2
Ulf Gitschthaler <ulf@gitschthaler.de>
2015-03-28 01:03:06 +0100

    add speclj config to project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,20 @@
 (defproject overseer "0.1.0-SNAPSHOT"
   :description "Learn clojure by creating simple bank statements parser and analyzer"
   :url "https://github.com/u6f6o/overseer"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.csv "0.1.2"]
                  [org.clojure/data.json "0.2.6"]]
+
   :main ^:skip-aot overseer.core
+  :profiles {:dev {:dependencies [[speclj "3.2.0"]]}
+             :uberjar {:aot :all}}
+
+  :plugins [[speclj "3.2.0"]]
+  :test-paths ["spec"]
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+
+)
